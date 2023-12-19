c999fe7903c03b9bbbe9a695866e585242558a39
brenosantin96 <brenolimaa96@gmail.com>
2023-12-19 20:45:11 +0100

    red

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
 "use client"
 
 import Square from '@/COMPONENTS/Square';
-import React, { useState } from 'react'
+import { listReducer } from '@/reducers/listReducer';
+import { Item } from '@/types/Item';
+import React, { useReducer, useState } from 'react'
 
 const Page = () => {
 
+    const [list, dispatch] = useReducer(listReducer, []);
 
+    const handleAddClick = () => {
+        dispatch({
+            type: 'add',
+            payload: {
+                text: 'NovoItemTaAqui'
+            }
+        })
+    }
+
+    
 
     return (
         <div className=''>
-            
-            
+
+            <button onClick={handleAddClick}>Adicionar</button>
+
         </div>
     )
 }
